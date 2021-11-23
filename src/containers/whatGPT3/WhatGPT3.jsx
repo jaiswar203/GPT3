4656f22e9f8eb7f3b92f14ab6bec1ddb0f8ae40c
Jaiswar <jaiswarnilesh2002@gmail.com>
2021-11-23 17:39:06 +0530

    updated

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const whatGPT3 = () => {
+    return (
+        <div>
+            <h1>WhatGPT3</h1>
+        </div>
+    )
+}
+
+export default whatGPT3
